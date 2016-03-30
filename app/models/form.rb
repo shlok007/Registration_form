9ef330a47529a618950f7fa3021cd94a22940ebc
Shlok Srivastava <shlok.srivastava15@gmail.com>
2016-03-30 11:19:59 +0000

    updated gemfile

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,2 +1,8 @@
 class Form < ActiveRecord::Base
+      validates :name, presence: true
+        validates :contact_no, presence: true
+          validates :date_of_birth, presence: true
+            validates :email, presence: true
+              validates :branch, presence: true
+      validates :year,presence: true
 end
